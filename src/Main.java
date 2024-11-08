9afacd8b2f0b23e64aaa34c250a348fc6b55c9f5
youngeiya <kim.ga.young467@gmail.com>
2024-11-08 17:32:55 +0900

    체스판 다시 칠하기

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,65 @@
-import java.io.*;
-import java.util.Arrays;
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static boolean[][] arr; //겹치는 값 boolean으로 확인
+    public static int min = 64;
 
-        int [] arr = new int[9];
-        int sum = 0;
+    public static void main(String[] args) {
 
-        for(int i = 0; i< 9; i++){ //9개의 수 모두  더하고 리스트에 저장하기 위한 배열 생성 앞쪽에
+        // N과 M의 값 입력받기
+        Scanner in = new Scanner(System.in);
 
-            int n = Integer.parseInt(br.readLine());
-            arr[i] = n;
-            sum += arr[i]; // 9개의 수 더한값 리스트 저장
+        int N = in.nextInt();
+        int M = in.nextInt();
+        // N과 M boolean방식으로 입력받음
+        arr = new boolean[N][M];
 
-        }
-        Arrays.sort(arr); //키 데이터 저장된 값 오름차순 배열
-
-        // 100을 넘기는 2명을 찾아서 빼줘야 함.. 어떻게? 이중 for문 사용
-        // 총합에서 빼줘야 함
-        int fake1 = 0,fake2=0;
-        for(int a=0; a<arr.length; a++){
-            for(int b= a +1; b<arr.length; b++){
-                if(sum - arr[a] - arr[b] == 100){
-                    fake1 = arr[a];
-                    fake2 = arr[b];
+        for (int i = 0; i < N; i++) {
+            String str = in.next();
+            for (int j = 0; j < M; j++) {
+                if (str.charAt(j) == 'W') {
+                    arr[i][j] = true;
+                } else {
+                    arr[i][j] = false;
                 }
             }
         }
-        for (int i = 0; i<arr.length; i++) {
-            if(fake1 == arr[i] || fake2 == arr[i]) continue;
-            System.out.println(arr[i]);
-        }
 
+        int N_row = N - 7;
+        int M_col = M - 7;
+
+        for (int i = 0; i < N_row; i++) {
+            for (int j = 0; j < M_col; j++) {
+                find(i, j);
+            }
+        }
+        System.out.println(min);
+    }
+
+    // 8*8 크기의 배열을 하나씩 검사하면서 첫칸색이 하얀색일 경우와 검은색일 경우의 수를 비교해서
+// 칠한 개수의 최소값을 구함
+    public static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int count = 0;
+
+        boolean TF = arr[x][y]; // 첫번째 칸의 색
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                // 올바른 색 아닐 경우 count1 증가
+                if (arr[i][j] != TF) {
+                    count++;
+                }
+                // 다음 칸 색 바꿔주기 true를 false로 false를 true로
+                TF = (!TF);
+            }
+            TF = !TF;
+        }
+        count = Math.min(count, 64 - count);
+
+        min = Math.min(min,count);
     }
 }
+
